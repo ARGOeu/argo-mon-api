05c4d4e7ea43ac93e0d43d8f904dc7dd12a1e162
cthermolia-grnet <77612126+cthermolia-grnet@users.noreply.github.com>
2025-11-21 16:00:26 +0200

    Revert "ARGO-5179 ARGO 5178 Create new tenant with general info / Read tenant"

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -32,7 +32,6 @@ According to [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) , the `Unr
 - [#9](https://github.com/ARGOeu/argo-mon-status-api/pull/9) ARGO-5158 Implement UserEndpoint for user registration and profile management
 - [#10](https://github.com/ARGOeu/argo-mon-status-api/pull/10) ARGO-5162 Implement Logo Upload Support for Status Pages
 - [#11](https://github.com/ARGOeu/argo-mon-status-api/pull/11) ARGO-5166 Implement entitlement-based access control for Status Pages API
-- [#12](https://github.com/ARGOeu/argo-mon-status-api/pull/12) ARGO-5179 ARGO-5178 Create new tenant with general info / Read tenant
 
 ### Fix
 
--- api/src/test/java/org/grnet/status/api/KeycloakTest.java
+++ api/src/test/java/org/grnet/status/api/KeycloakTest.java
@@ -3,8 +3,6 @@ package org.grnet.status.api;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.keycloak.client.KeycloakTestClient;
 import io.restassured.RestAssured;
-import jakarta.inject.Inject;
-import org.grnet.status.services.TenantService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 
@@ -16,14 +14,12 @@ import io.quarkus.test.common.http.TestHTTPResource;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class KeycloakTest {
 
-    @Inject
-    TenantService tenantService;
     @TestHTTPResource
     URI baseUri;
+
     protected String adminToken;
     protected String aliceToken;
     protected String bobToken;
-
 
     KeycloakTestClient keycloakClient = new KeycloakTestClient();
 
@@ -33,7 +29,6 @@ public class KeycloakTest {
         adminToken = getAccessToken("admin");
         aliceToken = getAccessToken("alice");
         bobToken = getAccessToken("bob");
-        tenantService.deleteAll();
     }
 
     protected String getAccessToken(String username) {
